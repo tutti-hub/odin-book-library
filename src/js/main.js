e8405fd13c4843d77014c62c2dbf0fd63b5b9a51
Dmitriy Tutichenko <tutichenko@gmail.com>
2024-05-14 19:07:22 +0300

    refactor: book list

--- src/js/main.js
+++ src/js/main.js
@@ -1,17 +1,12 @@
-let myLibrary = [];
-
 const titleInput = document.querySelector('#title');
 const authorInput = document.querySelector('#author');
 const pagesInput = document.querySelector('#pages');
 const readCheckBox = document.querySelector('#read');
 const addBookButton = document.querySelector('#add-book-btn');
 const booksListDiv = document.querySelector('.books-list');
-
-
 const dialog = document.querySelector('dialog');
 const showDialogButton = document.querySelector('#show-dialog-btn');
 const addBookDialogButton = document.querySelector('#add-book-dialog-btn');
-
 
 showDialogButton.addEventListener('click', (event) => {
     dialog.showModal();
@@ -19,43 +14,16 @@ showDialogButton.addEventListener('click', (event) => {
 
 addBookDialogButton.addEventListener('click', (event) => {
     dialog.close();
-
 });
-
-function createListItem(book, id) {
-    const bookListItem = document.createElement('div');
-    bookListItem.className = "book-list-item";
-
-    bookListItem.innerHTML = `<span data-book-id='${id}'>
-    ${book.title} by ${book.author},
-    ${book.pages} pages</span>`;
-
-    const checkBox = document.createElement("input");
-    checkBox.type = "checkbox";
-    checkBox.checked = book.read;
-    checkBox.dataset.bookId = id;
-    bookListItem.appendChild(checkBox);
-
-    bookListItem.insertAdjacentHTML('beforeend', `<button data-book-id='${id}' class="del-btn" type="button">Delete</button>`);
-
-    booksListDiv.appendChild(bookListItem);
-}
-
-function showBooks() {
-    booksListDiv.innerHTML = "";
-    myLibrary.forEach((e, i) => {
-        createListItem(e, i);
-    });
-}
 
 addBookDialogButton.addEventListener('click', (event) => {
     const book = new Book(titleInput.value,
                           authorInput.value,
                           pagesInput.value,
                           readCheckBox.checked);
-    addBookToLibrary(book);
-
-    showBooks();
+    const newBookId = addBookToLibrary(book);
+    const bookItem = createListItem(book, newBookId);
+    booksListDiv.appendChild(bookItem);
 
 });
 
@@ -63,14 +31,25 @@ booksListDiv.addEventListener('click', (event) => {
     if(event.target.classList.contains("del-btn")) {
         event.preventDefault();
         const id = event.target.dataset.bookId;
-        console.log("before delete ", id, myLibrary);
-        removeBook(id);
-        console.log("after delete ", id, myLibrary);
-        showBooks();
+        const book = findBookById(id);
+        if(confirm(`Delete book '${book.title}'?`)){
+
+            const itemToDelete = booksListDiv.querySelector('[data-book-id="' + id + '"]');
+            removeBook(id);
+            itemToDelete.remove();
+        }
+
+
     } else if(event.target.type === "checkbox") {
         toggleBookReadStatus(event.target.dataset.bookId);
     }
 });
+
+let myLibrary = [
+    new Book('Hobbit', 'Tolkien', 377),
+    new Book('Harry Potter', 'Joan Rolling', 666),
+    new Book('In America', 'Susan Sontag', 333)
+];
 
 function Book(title, author, pages, read=false) {
     this.title = title;
@@ -80,16 +59,56 @@ function Book(title, author, pages, read=false) {
 }
 
 function addBookToLibrary(book) {
-    myLibrary.push(book);
-    return book;
+    return myLibrary.push(book) - 1;
 }
 
 function removeBook(id) {
     myLibrary.splice(id, 1);
 }
 
+function findBookById(id) {
+    return myLibrary[id];
+}
+
 function toggleBookReadStatus(id) {
     myLibrary[id].read = !myLibrary[id].read;
 }
+
+function createListItem(book, id) {
+    const bookListItem = document.createElement('div');
+    bookListItem.className = "books-list-item";
+    bookListItem.dataset.bookId = id;
+
+    bookListItem.innerHTML = `<div class="books-list-item-title" >
+        ${book.title}
+    </div>
+    <div class="books-list-item-author">
+        ${book.author}
+    </div>
+    <div class="books-list-item-pages">
+        ${book.pages} pages
+    </div>
+    <div>
+        <input type="checkbox">
+        <span>Read this book</span>
+    </div>`;
+
+    const checkBox = bookListItem.querySelector('input[type="checkbox"]');
+    checkBox.checked = book.read;
+    checkBox.dataset.bookId = id;
+
+    bookListItem.insertAdjacentHTML('beforeend', `<button data-book-id='${id}' class="del-btn" type="button">Delete</button>`);
+
+    return bookListItem;
+}
+
+function showBooks() {
+    booksListDiv.innerHTML = "";
+    myLibrary.forEach((e, i) => {
+        booksListDiv.appendChild(createListItem(e, i));
+    });
+}
+
+showBooks();
 
 
